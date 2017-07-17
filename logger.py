aa8f5cfb189fe483b85963b73106cfe882165de6
Anthony Chen <anthony.gx.chen@gmail.com>
2017-07-17 02:47:14 -0400

    Added log file path

---
+++ logger.py
@@ -0,0 +1,9 @@
+"""
+Logger to keep track of some daily stats
+
+Note to self:
+    - file may be tab-separated instead of being commad separated
+
+"""
+
+LOG_FILE='/Users/Ant/Google Drive/Life_Related/Daily_Stat_Logs.csv'
