6e4ead1f0d6f8b86e723378d4d1c8498b5b227eb
Anthony Chen <anthony.gx.chen@gmail.com>
2017-07-23 11:26:33 -0400

    Wrote function typeConvert_sleepbot_rowEntry, which converts all the strings within a single sleepBot log into the correct objects

--- SleepBot_Utils.py
+++ SleepBot_Utils.py
@@ -76,16 +76,35 @@ def str2timedelta(str_duration):
 #   A list containing the same information, but in proper type. Note that the
 #   date may be changed based on the sleeptime
 # ===========================================================================
-def typeConvert_sleepbot_entry(logRow):
-    #Convert the input variables into their correct 
-    #Convert the sleeptime to a proper datetime
+def typeConvert_sleepbot_rowEntry(logRow):
+    #Convert the log date to a datetime.date
+    log_date = str2date(logRow[0])
+    #Convert the sleep and wake times to a datetime.time
+    sleep_time = str2time(logRow[1])
+    awake_time = str2time(logRow[2])
+    #Convert the duration to a datetime.timedelta
+    duration = str2timedelta(logRow[3])
+    #Try to convert the rating to an integer, else return a None
+    try:
+        rating = int(logRow[4])
+    except ValueError:
+        rating = None
+    #Return list with proepr variables
+    return [log_date, sleep_time, awake_time, duration, rating]
 
-    #Convert the waketime to a proper datetime
-    #Convert the duration to a proper duration
-    #Convert the rating to an integer
-    #Convert the date a proper date (maybe shift)
-    #Add things to a list and return
-    return None #TODO: temp return
+
+# ===========================================================================
+# Function correct a list containing log data
+#
+# Input:
+#   A list containing the date, sleep time, awake time, duration and rating
+#   for a particular sleepBot log, in its correct objects (i.e. datetime)
+#
+# Output:
+#   Same list, but correct data
+# ===========================================================================
+#def correct_log_data(log_data):
+    #
 
 
 # ===========================================================================
@@ -107,9 +126,15 @@ def read_sleepbot_logs(path):
     print "eyo"
 
     for row in logReader:
+        #Strip the elements of pre-existing quotes
+        str_row = [ entry.strip('\'').strip('\"') for entry in row ]
+        #Skip header
+        if 'Date' in str_row:
+            continue
+
+        log_data = typeConvert_sleepbot_rowEntry(str_row)
         print row
-        print len(row)
-        print typeConvert_sleepbot_entry(row)
+        print log_data
         break
         #Convert row to proper type
         #Append the row to a dataframe
